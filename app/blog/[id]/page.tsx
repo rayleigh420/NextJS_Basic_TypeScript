51b5f07e440cf64dcc4442dcd4a5f275b241a81a
rayleigh420 <nhatduy0409@gmail.com>
2023-04-05 21:05:22 +0700

    NextJS - Metadata

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,8 +1,14 @@
+import { Metadata } from "next";
 import Link from "next/link"
 
 interface BlogProp {
     params: { id: string }
 }
+
+export const metadata: Metadata = {
+    title: '...',
+    description: '...',
+};
 
 const Blog = ({ params }: BlogProp) => {
     return (
